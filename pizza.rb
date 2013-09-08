7218e463b19df30b1cd32ef29fcc8538a34e06eb
Eric Levine <djlevine@gmail.com>
2013-09-08 16:19:58 -0500

    Vegetarian attribute added, tests pass

--- pizza.rb
+++ pizza.rb
@@ -5,14 +5,15 @@ module Pizza
 
   class Topping
 
-    attr_accessor :name
-    
+    attr_accessor :name, :vegetarian
+
     # Instantiates a new topping
     # Name - The string name of the topping
     # Returns a new Topping object
 
-    def initialize(name)
+    def initialize(name, vegetarian: false)
       @name = name
+      @vegetarian = vegetarian
     end
 
   end
