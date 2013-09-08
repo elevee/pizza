9b74a112434ed682ec57bc318d7c0e7ad96f05be
Eric Levine <djlevine@gmail.com>
2013-09-08 15:35:07 -0500

    Topping initialize method passes

--- pizza.rb
+++ pizza.rb
@@ -5,5 +5,15 @@ module Pizza
 
   class Topping
 
+    attr_accessor :name
+    
+    # Instantiates a new topping
+    # Name - The string name of the topping
+    # Returns a new Topping object
+
+    def initialize(name)
+      @name = name
+    end
+
   end
 end
